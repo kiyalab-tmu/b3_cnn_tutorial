779a752a9cbbe9a34674933565525eaf94f71a24
Yuki Shiroma <46900773+marosiy@users.noreply.github.com>
2021-12-06 14:43:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -72,16 +72,7 @@ history = model.fit(train_images, train_labels, epochs=epoch, validation_split=0
 # ============
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2) #予測結果から正解率まで自動計算
 
-"""
-追加
-"""
-tf.keras.models.save_model(model, 'model.h5')
-predictions = model.predict(test_images) #予測結果のみ取得
-print(predictions[0]) #テストデータセットの1枚目の予測結果。(各数字である確率)
-print(predictions[0].argmax()) #テストデータセットの1枚目の予測結果。(何の数字か。)
-"""
-追加ここまで
-"""
+
 
 print('test_loss', test_loss)
 print('test_accuracy', test_acc)
@@ -113,3 +104,20 @@ plt.show()
 plt.savefig('accuracy_history.png')
 plt.clf()
 
+
+
+
+
+
+
+
+"""
+追加
+"""
+tf.keras.models.save_model(model, 'model.h5')
+predictions = model.predict(test_images) #予測結果のみ取得
+print(predictions[0]) #テストデータセットの1枚目の予測結果。(各数字である確率)
+print(predictions[0].argmax()) #テストデータセットの1枚目の予測結果。(何の数字か。)
+"""
+追加ここまで
+"""
